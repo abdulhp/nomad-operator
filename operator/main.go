d69235c1b2d67e25c4c44c899d21dc3e24ec20c0
abdulhp <hpabdul902@gmail.com>
2025-04-04 11:39:33 +0700

    refactor: adjust credentials

--- operator/main.go
+++ operator/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting Nomad backup operator...")
 
 	if err := run(os.Args[:]); err != nil {
 		fmt.Println(err)
@@ -18,11 +19,30 @@ func main() {
 }
 
 func run(args []string) error {
+	fmt.Println("Creating Nomad client...")
 
-	client, err := api.NewClient(&api.Config{})
+	client, err := api.NewClient(&api.Config{
+		// sandbox local credentials
+		Address:  "https://192.168.58.23:4646",
+		SecretID: "a492b42f-be94-a12d-4b5a-70f7a84f4d4f",
+		TLSConfig: &api.TLSConfig{
+			CACert:     "nomad99-certs/ca-cert.pem",
+			ClientCert: "nomad99-certs/cli-cert.pem",
+			ClientKey:  "nomad99-certs/cli-key.pem",
+		},
+	})
+
 	if err != nil {
 		return err
 	}
+
+	server, err := client.Agent().NodeName()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Connected to Nomad server:", server)
 
 	backup := NewBackup(client)
 	consumer := NewConsumer(client, backup.OnJob)
@@ -31,13 +51,14 @@ func run(args []string) error {
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		s := <-signals
-		fmt.Printf("Received %s, stopping\n", s)
-
+		fmt.Printf("Received %s, stopping Nomad backup operator...\n", s)
+		
 		consumer.Stop()
 		os.Exit(0)
 	}()
 
 	// blocks
+	fmt.Println("Listening Nomad Events...")
 	consumer.Start()
 	return nil
 }
